e5cbf2306953fb73fd729eb511f54c4faf36a98d
charley <charleyetienne.pro@gmail.com>
2020-08-07 16:38:09 +0200

    Improving signature

--- DataAccessObjectPattern/src/DAO/RoomDAO.ts
+++ DataAccessObjectPattern/src/DAO/RoomDAO.ts
@@ -3,6 +3,6 @@ import Room from "../Model/Room";
 export default interface RoomDAO {
     createRoom(): Room
     getAllRooms(): Array<Room>
-    getRoomById(id: string): Room
+    getRoomById(id: string): Room | undefined
     deleteRoomById(id: string): boolean
 }
